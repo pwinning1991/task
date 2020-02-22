a5b08c06880afa686d013c807cccd13adf8963b7
Philip Winnington <ftcpjw@yahoo.com>
2020-02-21 22:09:26 -0500

    adding intial db package

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/pwinning1991/task
 
 go 1.12
 
-require github.com/spf13/cobra v0.0.6
+require (
+	github.com/boltdb/bolt v1.3.1
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v0.0.6
+)
--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/pwinning1991/task/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/pwinning1991/task/cmd"
+	"github.com/pwinning1991/task/db"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()
+	dbpath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbpath))
+	must(cmd.RootCmd.Execute())
+}
 
+}
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
